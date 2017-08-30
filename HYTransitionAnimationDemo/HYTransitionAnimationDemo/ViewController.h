f327cd1965e3b8829525c95587032d7eadadabbe
oceanfive <849638313@qq.com>
2017-08-30 14:48:54 +0800

    1、初始化项目和配置podspec文件

---
+++ HYTransitionAnimationDemo/HYTransitionAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYTransitionAnimationDemo
+//
+//  Created by ocean on 2017/8/30.
+//  Copyright © 2017年 ocean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HYTransitionAnimationDemo/HYTransitionAnimationDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HYTransitionAnimationDemo
+//
+//  Created by ocean on 2017/8/30.
+//  Copyright © 2017年 ocean. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
